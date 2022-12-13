1f99db2e106bc721affc8addf19c86de74dc89aa
xottabbu4 <113995327+xottabbu4@users.noreply.github.com>
2022-12-14 01:50:55 +0200

    added SimpleLightBox libraries

--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,7 @@ const input = document.querySelector('.search-form-input');
 const btnSearch = document.querySelector('.search-form-button');
 const gallery = document.querySelector('.gallery');
 const btnLoadMore = document.querySelector('.load-more');
+let gallerySimpleLightbox = new SimpleLightbox('.gallery a');
 
 btnLoadMore.style.display = 'none';
 
@@ -27,7 +28,8 @@ if (trimmedValue !== '') {
         Notiflix.Notify.success(
           `Hooray! We found ${foundData.totalHits} images.`
         );
-        btnLoadMore.style.display = 'block';
+          btnLoadMore.style.display = 'block';
+          gallerySimpleLightbox.refresh();
       }
     });
   }
@@ -59,8 +61,9 @@ function renderImageList(images) {
   const markup = images
     .map(image => {
       console.log('img', image);
-      return `<div class="photo-card">
-       <img class="photo" src="${image.webformatURL}" alt="${image.tags}" loading="lazy"/>
+        return `<div class="photo-card">
+      
+       <a href="${image.largeImageURL}"><img class="photo" src="${image.webformatURL}" alt="${image.tags}" title="${image.tags}" loading="lazy"/></a>
         <div class="info">
            <p class="info-item">
     <b>Likes</b> <span class="info-item-api"> ${image.likes} </span>
